50e695dcb458f50fb94fd91abf40ea8541a01fa5
alagu-p <alagappan.plr@gmail.com>
2022-11-23 21:20:03 +0530

    Day-72

---
+++ login-screen/src/App.js
@@ -0,0 +1,14 @@
+import SignIn from './components/SignIn/SignIn.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SignIn/>
+
+     
+    </div>
+  );
+}
+
+export default App;
