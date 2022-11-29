8b031fa7fd928d25caf5ea2be340bbc561f9572e
azhagu379 <a.palanisamy@memob.com>
2022-11-29 19:36:47 +0530

    Day-78

--- youtue-clone-mui-reactjs/src/components/SearchBar.jsx
+++ youtue-clone-mui-reactjs/src/components/SearchBar.jsx
@@ -4,33 +4,37 @@ import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from "@mui/material";
 import SearchIcon from "../assets/svg/search-status.svg";
 import DeleteIcon from "@mui/icons-material/Delete";
+import "../../src/index.css";
 const SearchBar = () => {
   return (
     <Paper
       component="form"
       onSubmit={() => {}}
       sx={{
+        display: "flex",
         borderRadius: 20,
         border: "1px solid #e3e3e3",
         boxShadow: "none",
         mr: { sm: 5 },
       }}
     >
-      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
-        <img
-          src={SearchIcon}
-          alt="button"
-          style={{
-            fill: "#ccc",
-          }}
+      <Paper sx={{ borderRadius: 20, display: "flex" }}>
+        <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+          <img
+            src={SearchIcon}
+            alt="button"
+            style={{
+              fill: "#ccc",
+            }}
+          />
+        </IconButton>
+        <input
+          className="search-bar"
+          placeholder="Search....."
+          value=""
+          onChange={() => {}}
         />
-      </IconButton>
-      <input
-        className="search-bar"
-        placeholder="Search....."
-        value=""
-        onChange={() => {}}
-      />
+      </Paper>
     </Paper>
   );
 };
--- youtue-clone-mui-reactjs/src/components/SideBar.jsx
+++ youtue-clone-mui-reactjs/src/components/SideBar.jsx
@@ -1,7 +1,39 @@
 import { Stack } from "@mui/material";
+import { categories } from "../utils/constants";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
 
+const selectedCategory = "New";
 const Sidebar = () => {
-  return <Stack></Stack>;
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        overflowY: "auto",
+        height: { sx: "auto", md: "95%" },
+        flexDirection: { md: "column" },
+        justifyContent: "center",
+        display: "flex",
+        alignItems: "center",
+        marginLeft: "2px",
+      }}
+    >
+      {categories.map((categories) => (
+        <Avatar sx={{ display: "flex", marginBottom: "3px", padding: "2px" }}>
+          <Button
+            sx={{
+              bgcolor: "black",
+              background: categories.name === selectedCategory && "#FCFCFC",
+              color: "white",
+            }}
+            className="category-btn"
+          >
+            <span>{categories.icon}</span>
+          </Button>
+        </Avatar>
+      ))}
+    </Stack>
+  );
 };
 
 export default Sidebar;
